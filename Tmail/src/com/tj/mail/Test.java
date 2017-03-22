d289b0beadb2d283206493e2c691fdb4b1906466
levct <2303247590@qq.com>
2017-03-22 16:35:58 +0800

    修改Test类中的信息

--- Tmail/src/com/tj/mail/Test.java
+++ Tmail/src/com/tj/mail/Test.java
@@ -2,6 +2,6 @@ package com.tj.mail;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(Test.class.getResourceAsStream(""));
+		System.out.println("hello world");
 	}
 }
